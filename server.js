927a3142c53abde620118651a29b9d505567f31d
rorylkd <rory.donaldson14@gmail.com>
2021-06-08 14:13:37 +0800

    Added static folder middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const sequelize = require('./config/connection');
 
@@ -8,6 +9,8 @@ let PORT = process.env.PORT;
 if (PORT == null || PORT == "") {
   PORT = 8000;
 }
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port', PORT));
